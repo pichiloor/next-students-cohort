bb64b022cbbd83d1189184d0d92c30a427cae556
Andres Loor <andresloor@gmail.com>
2022-03-14 13:33:00 -0500

    Update index.tsx

--- src/components/StudentListItem/index.tsx
+++ src/components/StudentListItem/index.tsx
@@ -4,11 +4,27 @@ const StudentItem = (props: any) => {
   const student = props.student;
   return (
     <a href={`/student/${student.id}`}>{student?.first_name ?? ""}</a>
-    <Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
       <CardContent>
-         href={`/student/${student.id}`}>{student?.first_name ?? ""}
-      </CardContent>     
-     </Card>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }
 
